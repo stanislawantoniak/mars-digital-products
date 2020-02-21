a9bc9a32a1bec4f149d657f6d1c141cd6e6c037e
stanan <stanislaw.h.antoniak@gmail.com>
2020-02-21 08:13:19 +0100

    fix

--- src/pages/page-generic.js
+++ src/pages/page-generic.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 class PageGeneric extends Component {
 
