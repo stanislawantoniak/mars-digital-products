8c55ea6880da159b947fcf1f3e201c58c6871e86
stanan <stanislaw.h.antoniak@gmail.com>
2020-02-23 13:39:04 +0100

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,22 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  
-  <Layout>
-    <SEO title="Home" />
-    <h1>Welcome to Mars Digital World</h1>
-    <p>Welcome to your Mars Digital Product Experience Application.</p>
-    <p>This website is built using Gatsby, MuleSoft GraphQL enabled API.</p>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+	<Layout>
+		<SEO title="Home" />
+		<h1>Welcome to Mars Digital World</h1>
+		<p>Welcome to your Mars Digital Product Experience Application.</p>
+		<p>This website is built using Gatsby, MuleSoft GraphQL enabled API.</p>
 
-	<div>	
-    	<Link to="/page-2-static/" class="myButton">Static Graph QL Example</Link>
-    	<Link to="/page-1-gql/" class="myButton">Dynamic Graph QL Example</Link>
-    	<Link to="/page-3-rest/" class="myButton">Normal Rest Example</Link>
-	</div>
-	
-  </Layout>
+		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+			<Image />
+		</div>
+		<div>
+			<Link to="/page-2-static/" className="myButton">Static Graph QL Example</Link>
+			<Link to="/page-1-gql/" className="myButton">Dynamic Graph QL Example</Link>
+			<Link to="/page-3-rest/" className="myButton">Normal Rest Example</Link>
+		</div>
+	</Layout>
 )
 
 export default IndexPage
