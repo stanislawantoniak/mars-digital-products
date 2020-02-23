e53f04b44b59bd0e19357380059ea9ea1fb3fbfb
stanan <stanislaw.h.antoniak@gmail.com>
2020-02-23 14:31:37 +0100

    new navi

--- src/components/navi.js
+++ src/components/navi.js
@@ -3,11 +3,29 @@ import { Link } from "gatsby"
 import React from "react"
 
 const Navi = () => (
-
 	<div>
-		<Link to="/page-2-static/" className="myButton">Static Graph QL Example</Link>
-		<Link to="/page-1-gql/" className="myButton">Dynamic Graph QL Example</Link>
-		<Link to="/page-3-rest/" className="myButton">Normal Rest Example</Link>
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/page-2-static/">Static Graph QL Example</Link>
+				</li>
+				<li>
+					<Link to="/page-1-gql/">Dynamic Graph QL Example</Link>
+				</li>
+				<li>
+					<Link to="/page-3-rest/">Normal Rest Example</Link>
+				</li>
+			</ul>
+		</nav>
+
+		<div>
+			<Link to="/page-2-static/" className="myButton">Static Graph QL Example</Link>
+			<Link to="/page-1-gql/" className="myButton">Dynamic Graph QL Example</Link>
+			<Link to="/page-3-rest/" className="myButton">Normal Rest Example</Link>
+		</div>
 	</div>
 )
 export default Navi
