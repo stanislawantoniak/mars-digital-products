edbb2b4ef27ff40b49288fc05a7648a2f2d1515f
stanan <stanislaw.h.antoniak@gmail.com>
2020-02-24 11:29:24 +0100

    desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
 		<SEO title="Home" />
 		<h2>Welcome to Mars Digital World</h2>
 		<p>Welcome to your Mars Digital Product Experience Application.</p>
-		<p>This website is built using Gatsby framework based on NodeJs and is using MuleSoft GraphQL enabled API and Apollo GraphQL API based on NodeJs. Product data comes originally from Salsify (via Salsify API).</p>
+		<p>This website is built using Gatsby framework based on NodeJs and is pulling data from various sources: MuleSoft GraphQL enabled API, Apollo GraphQL API based on NodeJs, Rest API. Product data comes originally from Salsify (via Salsify API).</p>
 		<p>Select one of the options from menu bar to see how product data is rendered.</p>
 
 	</Layout>
