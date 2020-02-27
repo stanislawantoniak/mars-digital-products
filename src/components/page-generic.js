4446af6241258d43d4a006a37fc8132d9a881030
stanan <stanislaw.h.antoniak@gmail.com>
2020-02-27 17:13:12 +0100

    colors

--- src/components/page-generic.js
+++ src/components/page-generic.js
@@ -12,9 +12,7 @@ class PageGeneric extends Component {
 	}
 
 	toggleDataActive(){
-		
 		( this.state.dataActive == true? this.setState({ dataActive: false }) : this.setState({ dataActive: true }) );
-		
 		console.log('Toggle active to',this.state.dataActive);
 		
 	}
